8c18d79d8bd662f1282fce0462c0023c9612a5bd
Dhiraj9798 <115772569+Dhiraj9798@users.noreply.github.com>
2022-10-14 11:31:28 +0530

    Add files via upload

---
+++ 14 oct/DataTypeDemo2.java
@@ -0,0 +1,28 @@
+
+class DataTypeDemo2
+
+{	
+		public static void main(String args[])
+
+
+	{
+	String name="Dhiraj kumar";
+	char gender='M';
+	int age=21;
+	float marks=5.9f;
+	boolean pass=true;
+	
+	
+	
+	
+		System.out.println("Name:"+ name);
+				System.out.println("Gender:"+ gender);
+						System.out.println("Age:"+ age);
+								System.out.println("Pass:"+ pass);
+								
+								
+	}
+		
+	
+	
+}
